862e73dba70d61437c7af9d1b041c9418d8e49bb
sri raj <sriraj14@gmail.com>
2018-07-28 18:27:39 -0700

    Code added

---
+++ src/main/java/com/example/anuTestSpringBoot1/AnuTestSpringBoot1Application.java
@@ -0,0 +1,12 @@
+package com.example.anuTestSpringBoot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnuTestSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnuTestSpringBoot1Application.class, args);
+	}
+}
